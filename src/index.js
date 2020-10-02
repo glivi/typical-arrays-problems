373caae1a150c195bbb46d928beeeee6529e6f44
glivi <dekyaroslav@gmail.com>
2020-10-02 23:07:29 +0300

    feat: implement functions max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (arguments.length !== 0) {
+    if (array.length !== 0) {
+      return Math.min.apply(null, array);
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length !== 0) {
+    if (array.length !== 0) {
+      return Math.max.apply(null, array);
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length !== 0) {
+    if (array.length !== 0) {
+      return array.reduce((a, b) => a + b, 0) / array.length;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+  
 }
